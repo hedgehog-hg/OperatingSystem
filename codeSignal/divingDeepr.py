d387748b96c7a60df7f5c345814e941d0cb66930
donghee <debbiie952@gmail.com>
2024-07-30 23:51:03 +0900

    codeSignal 35

--- codeSignal/divingDeepr.py
+++ codeSignal/divingDeepr.py
@@ -1,4 +1,11 @@
 def extractEachKth(inputArray,k):
     return list(n for idx,n in enumerate(inputArray) if (idx+1)%k!=0 )
 
-print(extractEachKth([1,2,3,4,5,6,7,8,9,10],3))
+def firstDigit(inputString):
+    for s in inputString :
+        if s.isdigit() : return s
+    return 'no digit'
+    
+
+#print(extractEachKth([1,2,3,4,5,6,7,8,9,10],3))
+print(firstDigit('var_1__Int'))
