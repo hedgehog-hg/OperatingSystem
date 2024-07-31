d70f9a1453a7b5c9403047141c50ab38bbbddaa9
donghee <debbiie952@gmail.com>
2024-07-31 22:07:27 +0900

    codeSignal 37

--- codeSignal/divingDeepr.py
+++ codeSignal/divingDeepr.py
@@ -9,6 +9,15 @@ def firstDigit(inputString):
 def differentSymbolsNaive(s):
     return len(set(s))
 
+def arrayMaxConsecutiveSum(inputArray,k):
+    res = acc = sum(inputArray[:k])
+    for i in range(len(inputArray)-k) :
+        acc += inputArray[i+k] - inputArray[i]
+        res = max(res,acc)
+    return res
+
+
 #print(extractEachKth([1,2,3,4,5,6,7,8,9,10],3))
 #print(firstDigit('var_1__Int'))
-print(differentSymbolsNaive('cabca'))
+#print(differentSymbolsNaive('cabca'))
+print(arrayMaxConsecutiveSum([2,3,5,1,6],2))
