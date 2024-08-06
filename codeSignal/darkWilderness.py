8c0b98f090b97698ac2b6be0b5300e145b3b8510
donghee <debbiie952@gmail.com>
2024-08-06 22:52:43 +0900

    codeSignal 39

--- codeSignal/darkWilderness.py
+++ codeSignal/darkWilderness.py
@@ -6,5 +6,14 @@ def growingPlant(upSpeed,downSpeed,desireHeight):
     while upSpeed*day - downSpeed*(day-1) < desireHeight :
         day +=1
     return day
+def knapsackLight(v1,w1,v2,w2,maxW):
+    items = sorted([[v1,w1],[v2,w2]],key=lambda x:-x[0])
+    res = 0
+    for item in items:
+        if item[1] <= maxW : 
+            res += item[0]
+            maxW -= item[1]
+    return res
 
-print(growingPlant(100,10,910))
+#print(knapsackLight(10,5,6,4,9))
+#print(growingPlant(100,10,910))
