de79f0dde7764f27de197eefcbf49301abbc6414
donghee <debbiie952@gmail.com>
2024-08-10 23:04:59 +0900

    codeSignal 45

--- codeSignal/eruptionOfLight.py
+++ codeSignal/eruptionOfLight.py
@@ -10,8 +10,24 @@ def isBeautifulStirng(inputString) :
 
 def findEmailDomain(address):
     at= [idx for idx, x in enumerate(address) if x == '@']
-
     return address[at[-1]+1:]
 
-print(findEmailDomain("\"very.unusual.@.unusual.com\"@usual.com"))
+def buildPalindrome(st):
+    post =''
+    for idx in range(len(st)-1):
+        if st.count(st[idx]) > 1 and checkPalindrome(st[idx:]) :
+            break
+        else : 
+            post += st[idx]
+    return st+post[::-1]
+def checkPalindrome(str):
+    l = len(str)-1
+    for i in range(len(str)//2) :
+        if str[i] != str[l] :
+            return False
+        l-=1
+    return True
+
+print(buildPalindrome('abc'))
+#print(findEmailDomain("\"very.unusual.@.unusual.com\"@usual.com"))
 #print(isBeautifulStirng('zaa'))
