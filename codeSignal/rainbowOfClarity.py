604232ab7f190a2404afbd40ea58bbd34d4db347
donghee <debbiie952@gmail.com>
2024-08-10 23:59:48 +0900

    codeSignal 49

--- codeSignal/rainbowOfClarity.py
+++ codeSignal/rainbowOfClarity.py
@@ -1,4 +1,20 @@
 def isDigit(symbol):
     return all(s.isdigit() for s in symbol)
 
-print(isDigit('0'))
+def lineEncoding(s):
+    res = ''
+    prv = ''
+    cnt = 0
+    for c in s :
+        if prv and prv != c :
+            if cnt > 1 : res += str(cnt)
+            res += prv
+            cnt = 1
+        if not prv or prv == c :
+            cnt += 1
+        prv = c
+    if cnt > 1 : res += str(cnt)
+    return res + prv
+
+print(lineEncoding('aabbbc'))
+#print(isDigit('0'))
