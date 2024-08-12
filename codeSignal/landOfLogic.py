c53514f83f6262612e0a2b2f97951af9ce6931ea
donghee <debbiie952@gmail.com>
2024-08-12 23:23:09 +0900

    codeSignal 54

--- codeSignal/landOfLogic.py
+++ codeSignal/landOfLogic.py
@@ -14,5 +14,10 @@ def validTime(time) :
     h,m = map(int,time.split(':'))
     return 0<=h<24 and 0<=m<60
 
-print(validTime("13:58"))
+import re
+def sumUpNumbers(inputString):
+    return sum(int(x) for x in re.findall('\d+',inputString))
+
+print(sumUpNumbers('three (3) is not 4'))
+#print(validTime("13:58"))
 #print(longestWord('Ready'))
