0a5201685085dea0368bdd5d030bb4671dcc6d9f
donghee <debbiie952@gmail.com>
2024-08-12 23:12:07 +0900

    codeSignal 53

--- codeSignal/landOfLogic.py
+++ codeSignal/landOfLogic.py
@@ -9,4 +9,10 @@ def longestWord(text):
             tmp = ''
     arr.append(tmp)
     return max(arr, key=lambda x: len(x))
-print(longestWord('Ready'))
+
+def validTime(time) :
+    h,m = map(int,time.split(':'))
+    return 0<=h<24 and 0<=m<60
+
+print(validTime("13:58"))
+#print(longestWord('Ready'))
