b2c528b3d50b5026f5ec768c1bf31ee47a854cc7
donghee <debbiie952@gmail.com>
2024-08-13 23:01:27 +0900

    codeSignal 55

--- codeSignal/landOfLogic.py
+++ codeSignal/landOfLogic.py
@@ -18,6 +18,20 @@ import re
 def sumUpNumbers(inputString):
     return sum(int(x) for x in re.findall('\d+',inputString))
 
-print(sumUpNumbers('three (3) is not 4'))
+import numpy as np
+def differentSquares(matrix):
+    res = []
+    square = np.array(matrix)
+    for i in range(len(matrix)-1) :
+        for j in range(len(matrix[0])-1):
+            res.append(''.join(str(x) for x in square[i:i+2,j:j+2]))
+    return len(set(res))
+
+print(differentSquares([[1, 2, 1],
+          [2, 2, 2],
+          [2, 2, 2],
+          [1, 2, 3],
+          [2, 2, 1]]))
+#print(sumUpNumbers('three (3) is not 4'))
 #print(validTime("13:58"))
 #print(longestWord('Ready'))
