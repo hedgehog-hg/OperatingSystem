0a960c4231a29d069a74745cead37dbb08266820
donghee <debbiie952@gmail.com>
2024-08-14 21:24:10 +0900

    codeSignal 57

--- codeSignal/landOfLogic.py
+++ codeSignal/landOfLogic.py
@@ -28,11 +28,15 @@ def differentSquares(matrix):
     return len(set(res))
 
 def digitsProduct(product):
+    # product의 약수로 만들 수 있는 가장 작은 수 반환
+    # 예시 ) product 450 일때 2559 반환
+    # 약수는 0~9 사이의 값
     if product == 0:
         return 10
     res = []
     while product > 1 :
-        for i in range(9,1,-1):
+        # 9부터 내림 차순 ( 9로 나눠지면 3*3 도 가능 -> 33 보단 9 가 더 작다 )
+        for i in range(9,1,-1): 
             if product%i == 0:
                 res.append(i)
                 product/=i
@@ -40,7 +44,26 @@ def digitsProduct(product):
         else : return -1
     return ''.join(sorted(res))
 
-print(digitsProduct(450))
+def fileNaming(names):
+    res = []
+    for i in range(len(names)):
+        if names[i] in names[:i] or names[i] in res:
+            cnt = res.count(names[i])
+            tmp = names[i] + '(' + str(cnt) + ')'
+            while tmp in res :
+                cnt +=1
+                tmp = names[i] + '(' + str(cnt) + ')'
+            res.append(tmp)
+        else :
+            res.append(names[i])
+    return res
+
+print(fileNaming(["doc", 
+ "doc", 
+ "image", 
+ "doc(1)", 
+ "doc"]))
+#print(digitsProduct(450))
 # print(differentSquares([[1, 2, 1],
 #           [2, 2, 2],
 #           [2, 2, 2],
