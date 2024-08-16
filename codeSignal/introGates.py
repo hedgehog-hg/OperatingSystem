e258dfbc122c8d54a864e2a71b5f8b4d24126e10
donghee <debbiie952@gmail.com>
2024-08-16 23:19:45 +0900

    the core 4

--- codeSignal/introGates.py
+++ codeSignal/introGates.py
@@ -2,5 +2,8 @@ def addTwoDigits(n):
     return n//10 + n%10
 def largestNumber(n):
     return 10**n -1
+def candies(n,m):
+    return m//n *n
+def seatsInTheater(nCols,nRows,col,row):
+    return (nCols-col+1)*(nRows-row)
 
-print(largestNumber(2))
