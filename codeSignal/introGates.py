b6362ab02ceaf1fb4f557e86f810a45d959142d0
donghee <debbiie952@gmail.com>
2024-08-16 23:28:07 +0900

    the core 7

--- codeSignal/introGates.py
+++ codeSignal/introGates.py
@@ -8,3 +8,6 @@ def seatsInTheater(nCols,nRows,col,row):
     return (nCols-col+1)*(nRows-row)
 def maxMultiple(divisor,bound):
     return bound//divisor*divisor
+def circleOfNumbers(n,firstNumber):
+    mid = n//2
+    return mid + firstNumber if firstNumber<mid else firstNumber%mid
