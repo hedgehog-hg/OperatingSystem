da76ba0ec0ea6510253a2c316a5c3da36860147f
donghee <debbiie952@gmail.com>
2024-08-21 20:25:51 +0900

    the core 11

--- codeSignal/atTheCrossRoad.py
+++ codeSignal/atTheCrossRoad.py
@@ -10,6 +10,12 @@ def knapsackLight(value1,weight1,value2,weight2,maxW):
         if weight2 <= maxW :
             arr.append(value2)
         return max(arr) if arr else 0
+def extraNumber(a,b,c):
+    arr =[a,b,c]
+    for i in set(arr):
+        if arr.count(i) == 1:
+            return i
 
-print(knapsackLight(10,2,11,3,1))
+print(extraNumber(2,7,2))
+#print(knapsackLight(10,2,11,3,1))
 #print(reachNextLevel(10,15,5))
