0dd7112cfd82d63cba9849a8aa5fa99587594d9c
donghee <debbiie952@gmail.com>
2024-08-27 00:06:58 +0900

    the core 18

--- codeSignal/cornerOfOsand1s.py
+++ codeSignal/cornerOfOsand1s.py
@@ -2,6 +2,6 @@ def killKthBit(n,k):
     # n the Kth bit 0
     return n & ~(1 << (k - 1))
 def arrayPacking(a):
-    return int(''.join(list(bin(n)[2:]for n in a)[::-1]),2)
+    return int(''.join(list(format(n,'b').zfill(8)for n in a)[::-1]),2)
 print(arrayPacking([24,85,0]))
 #print(killKthBit(37,4))
