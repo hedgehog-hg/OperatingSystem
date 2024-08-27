747c87d0ff5d3a4a6a91d8e78f62ccec9b01ea6a
donghee <debbiie952@gmail.com>
2024-08-27 19:19:47 +0900

    the core 20

--- codeSignal/cornerOfOsand1s.py
+++ codeSignal/cornerOfOsand1s.py
@@ -5,7 +5,9 @@ def arrayPacking(a):
     return int(''.join(list(format(n,'b').zfill(8)for n in a)[::-1]),2)
 def rangeBitCount(a,b):
     return sum(bin(i).count('1') for i in range(a,b+1))
-
-print(rangeBitCount(2,7))
+def mirrorBits(a):
+    return int(bin(a)[2:][::-1],2)
+print(mirrorBits(97))
+#print(rangeBitCount(2,7))
 #print(arrayPacking([24,85,0]))
 #print(killKthBit(37,4))
