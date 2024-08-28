c78f52873014b0dd9efbbf2b16cadc1b063d2143
donghee <debbiie952@gmail.com>
2024-08-28 17:15:59 +0900

    the core 22

--- codeSignal/cornerOfOsand1s.py
+++ codeSignal/cornerOfOsand1s.py
@@ -7,10 +7,13 @@ def rangeBitCount(a,b):
     return sum(bin(i).count('1') for i in range(a,b+1))
 def mirrorBits(a):
     return int(bin(a)[2:][::-1],2)
-
 def secondRightmostZeroBit(n):
     return 2**list(i for i,e in enumerate(format(n,'b')[::-1]) if e == '0')[1]
-print(secondRightmostZeroBit(37))
+def swapAdjacenctBits(n):
+    return int(''.join(list(format(n,'b').zfill(32)[i:i+2][::-1] for i in range(0,33,2))),2)
+
+print(swapAdjacenctBits(13))
+#print(secondRightmostZeroBit(37))
 #print(mirrorBits(97))
 #print(rangeBitCount(2,7))
 #print(arrayPacking([24,85,0]))
