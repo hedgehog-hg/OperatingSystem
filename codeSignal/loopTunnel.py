dc1e444d9b978d568d1dd0c4804d2a1e872e8130
donghee <debbiie952@gmail.com>
2024-08-29 21:52:26 +0900

    the core 27

--- codeSignal/loopTunnel.py
+++ codeSignal/loopTunnel.py
@@ -8,5 +8,9 @@ def leastFactorial(n):
 import math
 def countSumOfTwoRepresent(n,l,r):
     return math.ceil(len(set(sum(list([i,n-i] for i in range(l,r+1) if l<=n-i <= r),[])))/2)
+def magicalWell(a,b,n):
+    print((n-1)*(a+b))
+    return n*a*b + sum(range(n))*(a+b) + sum(i**2 for i in range(1,n))
 
-print(countSumOfTwoRepresent(6,2,4))
+print(magicalWell(6,5,3))
+#print(countSumOfTwoRepresent(6,2,4))
