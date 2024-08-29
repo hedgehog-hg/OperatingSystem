5af33f80a1e1e29dab80da21e6a22e5ab35f5ac0
donghee <debbiie952@gmail.com>
2024-08-29 21:26:14 +0900

    the core 26

--- codeSignal/loopTunnel.py
+++ codeSignal/loopTunnel.py
@@ -5,3 +5,8 @@ def leastFactorial(n):
         f*=k
         k+=1
     return f
+import math
+def countSumOfTwoRepresent(n,l,r):
+    return math.ceil(len(set(sum(list([i,n-i] for i in range(l,r+1) if l<=n-i <= r),[])))/2)
+
+print(countSumOfTwoRepresent(6,2,4))
